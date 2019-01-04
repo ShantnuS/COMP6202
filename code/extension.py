d149eb6e3db10a980e269cb43e6cdb32c5b41117
ShantnuS <shantnu@outlook.com>
2019-01-04 12:23:26 +0000

    extension changes

--- code/extension.py
+++ code/extension.py
@@ -2,11 +2,11 @@ from random import shuffle
 import csv
 
 Gmax = 0.02 #max G ( a 00000000 individual)
-Gbase = 0.018 #base G  (a 11111111 individual)
+Gbase = 0.019 #base G  (a 11111111 individual)
 Cmax = 0.2 #max C ( a 00000000 individual)
-Cbase = 0.1 #base C ( a 11111111 individual)
+Cbase = 0.15 #base C ( a 11111111 individual)
 N = 4000 #population size 
-T = 150 #number of generations 
+T = 200 #number of generations 
 K = 0.1 #death rate
 R_small = 4 #R for small groups
 R_large = 50 #R for large groups
@@ -35,20 +35,20 @@ def sort_individual(individual):
 
 def get_genotypes():
     genotypes = []
-    for i in range(allele_size):
-            individual = ("1"*(i+1)).zfill(allele_size)
-            genotypes.append(individual+"1")
-            genotypes.append(individual+"0")
+    for i in range(allele_size+1):
+            individual = ("1"*(i)).zfill(allele_size)
+            genotypes.append(individual+"L")
+            genotypes.append(individual+"S")
     return genotypes
 
 #Initialise migrant pool
 def initialise_pool():
     pool = []
     while len(pool)<N:
-        for i in range(allele_size):
-            individual = ("1"*(i+1)).zfill(allele_size)
-            pool.append(individual+"1")
-            pool.append(individual+"0")
+        for i in range(allele_size+1):
+            individual = ("1"*(i)).zfill(allele_size)
+            pool.append(individual+"L")
+            pool.append(individual+"S")
 
     shuffle(pool)
     return pool
@@ -58,7 +58,7 @@ def create_groups(migrant_pool):
     large_group = []
     small_group = []
     for individual in migrant_pool:
-        if individual.endswith("1"):
+        if individual.endswith("L"):
             large_group.append(individual)
         else:
             small_group.append(individual)
@@ -189,7 +189,9 @@ def run():
     migrant_pool = initialise_pool()
     initialise_output(output_file,genotypes,migrant_pool)
 
-    for _ in range(T):
+    for i in range(T):
+        print(i)
+        print(len(migrant_pool))
         #Form Groups
         groups = create_groups(migrant_pool)
 
--- code/test.py
+++ code/test.py
@@ -1,8 +1,38 @@
-allele_size = 8
+Gmax = 0.02 #max G ( a 00000000 individual)
+Gbase = 0.018 #base G  (a 11111111 individual)
+Cmax = 0.2 #max C ( a 00000000 individual)
+Cbase = 0.1 #base C ( a 11111111 individual)
+N = 4000 #population size 
+T = 150 #number of generations 
+K = 0.1 #death rate
+R_small = 4 #R for small groups
+R_large = 50 #R for large groups
+t = 4 #time spent in groups before dispersal 
+L_size = 40 #size of the large group
+S_size = 4 #size of the small group
+allele_size = 8 #bitstring size not including Size parameter 
+
 
 def sort_individual(individual):
     genotype = list(individual[:allele_size])
     genotype.sort()
     return ''.join(genotype)+individual[-1:]
 
-print(sort_individual("111010010"))
+def calculate_G(individual):
+    steps = float((Gmax-Gbase)/allele_size)
+    return Gbase+(list(individual[:allele_size]).count("0"))*steps
+
+def calculate_C(individual):
+    steps = float((Cmax-Cbase)/allele_size)
+    return Cbase+(list(individual[:allele_size]).count("0"))*steps
+
+def get_genotypes():
+    genotypes = []
+    for i in range(allele_size+1):
+            individual = ("1"*(i)).zfill(allele_size)
+            genotypes.append(individual+"L")
+            genotypes.append(individual+"S")
+    return genotypes
+
+for i in get_genotypes():
+    print(i + " | " + "C: " + str(calculate_C(i)) + " | " + "G: " + str(calculate_G(i)))
